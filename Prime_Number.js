ee19d8fdc617e30e98ec25f071a4c838dfa126d1
Juri Kalita <jurikalita011@gmail.com>
2022-11-02 12:45:10 +0530

    prime code updated

--- Prime_Number.js
+++ Prime_Number.js
@@ -1,8 +1,17 @@
 function PrimeNumber(num){
 
   let factor=0;
-  for(let i=1;i<=num;i--){
+  for(let i=1;i<=num;i++){
     if(num%i==0){
       factor++;
     }
   }
+  if(factor==2){
+      return true;
+    }else{
+      return false;
+    }
+}
+
+ let ans=PrimeNumber(8);
+ console.log(ans);
